919d5649492bbf362b840339b2daa51764a41286
mikkaiscoding <mikka.kantinti@gmail.com>
2023-10-15 15:39:39 +0200

    added info

---
+++ variables_et_declarations.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int v; // int v; c’est une déclaration de variable  variable : case mémoire de l’ordinateur possédant une taille, un nom et une valeur
+    v = 3;
+    printf("on a v= %i", v);
+    return 0;
+}
